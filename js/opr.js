f2f14c3a712e8384c5eae1baff7f259db340b224
Florian Schlittenbauer <fschlittenbauer@1drop.de>
2014-12-12 16:45:56 +0100

    initial commit

---
+++ js/opr.js
@@ -0,0 +1,17 @@
+var opr_cur = $('current');
+function opr_next(){
+
+}
+function opr_prev(){
+
+}
+function opr_hasNext(slide){
+
+}
+function opr_hasPrev(slide){
+
+}
+
+$('.next').click( function(){
+    $(this).closest('.page').slideUp();
+});
