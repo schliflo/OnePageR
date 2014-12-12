fb55a3302bf936efbb5cdfc36ede2d2376198531
Florian Schlittenbauer <fschlittenbauer@1drop.de>
2014-12-12 17:32:33 +0100

    some scripting

--- js/opr.js
+++ js/opr.js
@@ -1,17 +1,40 @@
-var opr_cur = $('current');
+var opr_cur = $('.current');
 function opr_next(){
-
+    var slide = opr_cur;
+    var next = slide.next('.page');
+    if( next != slide ){
+        slide.addClass('animateout');
+        slide.removeClass('current');
+        next.addClass('current');
+        slide.slideUp(function() {
+            slide.removeClass('animateout');
+            slide.show();
+        });
+        opr_cur = next;
+    }
 }
 function opr_prev(){
+    var slide = opr_cur;
+    var prev = slide.prev('.page');
+    if( prev != slide ){
 
-}
-function opr_hasNext(slide){
-
-}
-function opr_hasPrev(slide){
-
+    }
 }
 
 $('.next').click( function(){
-    $(this).closest('.page').slideUp();
+    opr_next();
+});
+
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 38: // up
+            opr_prev();
+            break;
+        case 40: // down
+            opr_next();
+            break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
 });
